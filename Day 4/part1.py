c214b4534524fb2c9c83b5bdc72a708eb94cd263
frsouzaa <felipe.souza1506@outlook.com>
2023-12-09 00:19:04 -0300

    solving Part 1 of Day 4

---
+++ Day 4/part1.py
@@ -0,0 +1,19 @@
+from typing import List, Set
+import re
+
+
+with open('Day 4/input.txt') as f:
+    lines: List[str] = f.readlines()
+
+total_sum: int = 0
+
+for line in lines:
+    res = re.search(r"(Card +\d+: )([\d ]+)(\|)([\d ]+)", line)
+    if not res:
+        continue
+    winning: Set[str] = set(res[2].strip().replace("  ", " ").split(" "))
+    have: Set[str] = set(res[4].strip().replace("  ", " ").split(" "))
+    if length := len(have.intersection(winning)):
+        total_sum += 2**(length-1)
+        
+print(total_sum)
